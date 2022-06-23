4f69628c7e1267a50a600614bea0d3117bed3f9d
mrdja99 <markorsb@gmail.com>
2022-06-24 00:44:00 +0200

    Dodati kontroler za autentifikaciju i rute za register i login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\GledalacController;
 use App\Http\Controllers\FilmController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,12 +21,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/gledalac', [GledalacController::class, 'index']);
-Route::get('/gledalac/{id}', [GledalacController::class, 'show']);
-Route::post('/gledalac', [GledalacController::class, 'store']);
-Route::put('/gledalac/{id}', [GledalacController::class, 'update']);
-Route::delete('/gledalac/{id}', [GledalacController::class, 'destroy']);
 
-Route::get('/film', [FilmController::class, 'index']);
-Route::get('/film/{id}', [FilmController::class, 'show']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('/gledalac', [GledalacController::class, 'index']);
+    Route::get('/gledalac/{id}', [GledalacController::class, 'show']);
+    Route::post('/gledalac', [GledalacController::class, 'store']);
+    Route::put('/gledalac/{id}', [GledalacController::class, 'update']);
+    Route::delete('/gledalac/{id}', [GledalacController::class, 'destroy']);
+
+    Route::get('/film', [FilmController::class, 'index']);
+    Route::get('/film/{id}', [FilmController::class, 'show']);
+
+});
 
